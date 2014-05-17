79e66675890607db626aff398c8f4da55d49ecef
Graffenberger <s0534570@htw-berlin.de>
2014-05-17 13:54:46 +0200

    Create Game

--- minilotto/gen/com/minilotto_version1/R.java
+++ minilotto/gen/com/minilotto_version1/R.java
@@ -14,23 +14,36 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int ListView=0x7f060005;
-        public static final int TextView01=0x7f060002;
-        public static final int TextView02=0x7f06000c;
-        public static final int TextView03=0x7f060009;
-        public static final int btnMinilotto_Spielen=0x7f060007;
-        public static final int btnMitspielen=0x7f060006;
-        public static final int btnNeuSpiel=0x7f060000;
-        public static final int btnPrivate_Informationen=0x7f06000b;
-        public static final int textView1=0x7f060003;
-        public static final int textView2=0x7f060004;
-        public static final int txtHinweis=0x7f06000a;
-        public static final int txtInformation=0x7f060001;
-        public static final int txtSpiel_Hinweis=0x7f060008;
+        public static final int ListView=0x7f060010;
+        public static final int TextView01=0x7f06000d;
+        public static final int TextView02=0x7f060017;
+        public static final int TextView03=0x7f060014;
+        public static final int btnErstellen=0x7f06000c;
+        public static final int btnLogin=0x7f060003;
+        public static final int btnMinilotto_Spielen=0x7f060012;
+        public static final int btnMitspielen=0x7f060011;
+        public static final int btnNeuSpiel=0x7f06000e;
+        public static final int btnPrivate_Informationen=0x7f060016;
+        public static final int btnRegister=0x7f060002;
+        public static final int editEinsatz=0x7f060009;
+        public static final int editMaxSpieler=0x7f060006;
+        public static final int editPassword=0x7f060000;
+        public static final int editUsername=0x7f060001;
+        public static final int imageView1=0x7f060004;
+        public static final int radioGroup=0x7f06000a;
+        public static final int textView1=0x7f060005;
+        public static final int textView2=0x7f060007;
+        public static final int textView3=0x7f060008;
+        public static final int textView5=0x7f06000b;
+        public static final int txtHinweis=0x7f060015;
+        public static final int txtInformation=0x7f06000f;
+        public static final int txtSpiel_Hinweis=0x7f060013;
     }
     public static final class layout {
-        public static final int spiel_uebersicht=0x7f030000;
-        public static final int welcome_layout=0x7f030001;
+        public static final int activity_main=0x7f030000;
+        public static final int new_spiel_erstellen=0x7f030001;
+        public static final int spiel_uebersicht=0x7f030002;
+        public static final int welcome_layout=0x7f030003;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
