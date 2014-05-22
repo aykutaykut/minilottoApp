a07f6881f4ea7b3dec261c3ab903914bac1628fd
Graffenberger <s0534570@htw-berlin.de>
2014-05-22 23:14:29 +0200

    SpielLotto

---
+++ minilotto/src/com/minilotto_version1/SpielLottoActivity.java
@@ -0,0 +1,64 @@
+package com.minilotto_version1;
+
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import org.ksoap2.SoapEnvelope;
+import org.ksoap2.serialization.MarshalFloat;
+import org.ksoap2.serialization.SoapObject;
+import org.ksoap2.serialization.SoapPrimitive;
+import org.ksoap2.serialization.SoapSerializationEnvelope;
+import org.ksoap2.transport.HttpTransportSE;
+
+import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.StrictMode;
+
+public class SpielLottoActivity extends ActionBarActivity {
+	
+	public final String NAMESPACE="http://AndroidMinilottoDatabaseService.com/";
+	public final String URL="http://viendatabaseservice.somee.com/Webservice.asmx?WSDL";
+	
+	private String Ergebnis_String;
+	private String VorratErgebnis_String;
+	private boolean Gewinner_Gefunden;
+	private boolean einmal_ausführen_drücken=false;
+	
+	
+	private Button ausführen,refresh;
+	private EditText z1,z2,z3;
+	private TextView Ergebnis;
+	
+	public TextView Information;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.ergebnisse_layout);
+		
+		
+		StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+		StrictMode.setThreadPolicy(policy);
+		
+		ausführen = (Button) findViewById(R.id.btnAusfuehren);
+		refresh = (Button) findViewById(R.id.btnRefresh);
+		
+		z1 = (EditText) findViewById(R.id.editZahl1);
+		z2 = (EditText) findViewById(R.id.editZahl2);
+		z3 = (EditText) findViewById(R.id.editZahl3);
+		
+		Information = (TextView) findViewById(R.id.textView2);
+		Ergebnis = (TextView) findViewById(R.id.txtErgebnis_zeigen);
+		
+		
+	}
